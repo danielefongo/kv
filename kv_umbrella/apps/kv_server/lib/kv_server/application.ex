7a345d57b3df04f3f634406b38bc4e6c347b6712
danielefongo <danielefongo@gmail.com>
2020-03-04 15:51:28 +0100

    add server submodule

---
+++ kv_umbrella/apps/kv_server/lib/kv_server/application.ex
@@ -0,0 +1,10 @@
+defmodule KVServer.Application do
+  use Application
+
+  def start(_type, _args) do
+    children = []
+
+    opts = [strategy: :one_for_one, name: KVServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ kv_umbrella/apps/kv_server/test/kv_server_test.exs
@@ -0,0 +1,7 @@
+defmodule KVServerTest do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert KVServer.hello() == :world
+  end
+end
