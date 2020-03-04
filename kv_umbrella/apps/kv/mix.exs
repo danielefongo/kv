9f44593f1ce7d5c01cdddafb205302d883cb19a5
danielefongo <danielefongo@gmail.com>
2020-03-04 16:19:31 +0100

    move kv as umbrella submodule

--- kv_umbrella/apps/kv/mix.exs
+++ kv_umbrella/apps/kv/mix.exs
@@ -6,12 +6,15 @@ defmodule KV.MixProject do
       app: :kv,
       version: "0.1.0",
       elixir: "~> 1.9",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +22,7 @@ defmodule KV.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    []
   end
 end
