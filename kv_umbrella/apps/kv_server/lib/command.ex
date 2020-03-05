4e1f0e4cf6066d9eec7d2dafaf6c5c09c73aee94
danielefongo <danielefongo@gmail.com>
2020-03-05 12:31:54 +0100

    add fake responses on server

---
+++ kv_umbrella/apps/kv_server/lib/command.ex
@@ -0,0 +1,32 @@
+defmodule KVServer.Command do
+  @doc ~S"""
+  Parses the given `line` into a command.
+
+  ## Examples
+
+    iex> KVServer.Command.parse "CREATE shopping\r\n"
+    {:ok, {:create, "shopping"}}
+
+    iex> KVServer.Command.parse "CREATE  shopping  \r\n"
+    {:ok, {:create, "shopping"}}
+
+    iex> KVServer.Command.parse "PUT shopping milk 1\r\n"
+    {:ok, {:put, "shopping", "milk", "1"}}
+
+    iex> KVServer.Command.parse "GET shopping milk\r\n"
+    {:ok, {:get, "shopping", "milk"}}
+
+    iex> KVServer.Command.parse "DELETE shopping eggs\r\n"
+    {:ok, {:delete, "shopping", "eggs"}}
+  """
+
+  def parse(line) do
+    case String.split line do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+end
--- kv_umbrella/apps/kv_server/test/kv_server_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule KVServerTest do
-  use ExUnit.Case
-
-  test "greets the world" do
-    assert KVServer.hello() == :world
-  end
-end
